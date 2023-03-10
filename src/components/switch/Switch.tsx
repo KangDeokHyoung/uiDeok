28e1045a85af0a6afd31cc0827fca8479b765ad7
KangDeokHyoung <ejrgud2003@naver.com>
2023-03-11 00:48:12 +0900

    switch componet onChange value값 외부로 넘길수 있도록 로직 변경중

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -1,26 +1,29 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { IoMdRadioButtonOff, IoMdRadioButtonOn } from "react-icons/io";
 import classNames from "classnames";
 
 type Props = {
-  value?: number | boolean;
   disabled?: boolean;
   onChange?: (value: number) => void;
+  value?: number | boolean;
 };
 
 export function Switch(props: Props) {
-  const { value, disabled, onChange } = props;
-  const [toggle, setToggle] = useState(value);
+  const { disabled, onChange = () => false, value } = props;
+  const [toggle, setToggle] = useState(0);
+
+  const check = value ?? toggle;
 
   const onChangeHandler = () => {
     if (disabled) return;
     const changeHandler = onChange ?? setToggle;
-    changeHandler(value ? 0 : 1);
+    changeHandler(check ? 0 : 1);
+    if (!value) setToggle(check ? 0 : 1);
   };
 
   return (
     <label id="ui_switch" onClick={onChangeHandler}>
-      <input type="checkbox" checked={!toggle} />
+      <input type="checkbox" />
       <div className="slider" />
     </label>
   );
--- src/pages/switch/SwitchPage.tsx
+++ src/pages/switch/SwitchPage.tsx
@@ -4,13 +4,15 @@ import { FlexBox } from "common";
 import { Switch } from "components";
 
 export const SwitchPage = () => {
+  const [s, ss] = React.useState(0);
+  console.log({ s });
   return (
     <div id="switch-page">
       <h4>Switch</h4>
       <p>설명</p>
       <ContentBox>
         <FlexBox st={{ gap: 20 }}>
-          <Switch />
+          <Switch onChange={(item) => ss(item)} value={s} />
         </FlexBox>
       </ContentBox>
     </div>
