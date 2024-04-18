6a029c77dd8095c8ce3f4725f1f599b980b073c8
deok9 <deok9@useb.co.kr>
2024-04-18 11:11:11 +0900

    feature/toast - toast component 제작 완료

--- src/pages/toast/ToastPage.tsx
+++ src/pages/toast/ToastPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { CodeMirrorContext } from "common";
-import { Button, Tooltip } from "components";
+import { Button } from "components";
 import { ContentBox } from "layout";
 import source from "./source";
 import Toast from "components/toast/Toast";
@@ -19,7 +19,7 @@ export const ToastPage = () => {
       <ContentBox>
         <Button onClick={onClickHandler}>Toast</Button>
       </ContentBox>
-      {/* <CodeMirrorContext source={source} title="Tooltip" /> */}
+      <CodeMirrorContext source={source} title="Toast" />
     </div>
   );
 };
